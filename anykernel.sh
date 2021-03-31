39256cf6b047b14343d2a73dc0ae45134237cf9d
Stephen Xian <stevexian56@gmail.com>
2021-03-31 23:40:13 +0800

    Anykernel.sh: allow overriding refreshrate
    Set 84Hz by default

--- anykernel.sh
+++ anykernel.sh
@@ -33,6 +33,18 @@ ramdisk_compression=auto;
 ## AnyKernel install
 split_boot;
 
+## Screen OC
+patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=6"
+fr=$(cat /sdcard/framerate_override | tr -cd "[0-9]");
+[ $fr -eq 66 ] && ui_print "   Setting 66 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=1"
+[ $fr -eq 69 ] && ui_print "   Setting 69 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=2"
+[ $fr -eq 72 ] && ui_print "   Setting 72 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=3"
+[ $fr -eq 75 ] && ui_print "   Setting 75 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=4"
+[ $fr -eq 81 ] && ui_print "   Setting 81 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=5"
+[ $fr -eq 84 ] && ui_print "   Setting 84 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=6"
+[ $fr -eq 90 ] && ui_print "   Setting 90 Hz refresh rate" && patch_cmdline "msm_drm.framerate_override" "msm_drm.framerate_override=7"
+rm /sdcard/framerate_override
+
 mv $home/rd-new.cpio $home/ramdisk-new.cpio
 
 flash_boot;
