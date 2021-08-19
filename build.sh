4f17648f8e3c982e982c566e1449d4e53ee1c1ab
EcrosoftXiao <xy19990903@gmail.com>
2021-08-19 15:54:52 +0800

    scripts: build: Refactor

--- build.sh
+++ build.sh
@@ -2,9 +2,13 @@ DATE=$(date +"%Y%m%d")
 VERSION=$(git rev-parse --short HEAD)
 KERNEL_NAME=Evasi0nKernel-cepheus-"$DATE"
 
-KERNEL_PATH=$PWD
-CLANG_PATH=~/clang-proton
-ANYKERNEL_PATH=~/Anykernel3
+export KERNEL_PATH=$PWD
+export ANYKERNEL_PATH=~/Anykernel3
+export CLANG_PATH=~/proton-clang
+export PATH=${CLANG_PATH}/bin:${PATH}
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export ARCH=arm64
+export SUBARCH=arm64
 
 echo "===================Setup Environment==================="
 git clone --depth=1 https://github.com/kdrag0n/proton-clang $CLANG_PATH
@@ -13,18 +17,17 @@ sh -c "$(curl -sSL https://github.com/akhilnarang/scripts/raw/master/setup/andro
 
 echo "=========================Clean========================="
 rm -rf $KERNEL_PATH/out/ *.zip
-make mrproper
+make mrproper && git reset --hard HEAD
 
 echo "=========================Build========================="
-export ARCH=arm64
-export SUBARCH=arm64
-make O=out cepheus_defconfig
-make -j4 O=out CC=$CLANG_PATH/bin/clang CROSS_COMPILE=$CLANG_PATH/bin/aarch64-linux-gnu- CROSS_COMPILE_ARM32=$CLANG_PATH/bin/arm-linux-gnueabi-
+make O=out CC="ccache clang" CXX="ccache clang++" CROSS_COMPILE=$CLANG_PATH/bin/aarch64-linux-gnu- CROSS_COMPILE_ARM32=$CLANG_PATH/bin/arm-linux-gnueabi- LD=ld.lld cepheus_defconfig
+make O=out CC="ccache clang" CXX="ccache clang++" CROSS_COMPILE=$CLANG_PATH/bin/aarch64-linux-gnu- CROSS_COMPILE_ARM32=$CLANG_PATH/bin/arm-linux-gnueabi- LD=ld.lld 2>&1 | tee kernel.log
 
 if [ ! -e $KERNEL_PATH/out/arch/arm64/boot/Image.gz-dtb ]; then
     echo "=======================FAILED!!!======================="
-    rm -rf $ANYKERNEL_PATH $CLANG_PATH $KERNEL_PATH/out/
+    rm -rf $ANYKERNEL_PATH $KERNEL_PATH/out/
     make mrproper>/dev/null 2>&1
+    git reset --hard HEAD 2>&1
     exit -1>/dev/null 2>&1
 fi
 
@@ -36,6 +39,6 @@ cd $ANYKERNEL_PATH
 zip -r $KERNEL_NAME *
 mv $KERNEL_NAME.zip $KERNEL_PATH/out/
 cd $KERNEL_PATH
-rm -rf $CLANG_PATH
+#rm -rf $CLANG_PATH
 rm -rf $ANYKERNEL_PATH
 echo $KERNEL_NAME.zip
